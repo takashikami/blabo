f8380ff2121fbf490307213a75cbf9bac17d3740
Takashi KAMI <takashikami@gmail.com>
2014-05-20 20:36:02 +0900

    implements files_controller#view

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :subjects
+  get 'files/:id' => 'files#view'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
