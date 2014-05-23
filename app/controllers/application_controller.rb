18a1d9897f3dfbf7705f3eb71ad33e5bbc24d3a4
Takashi KAMI <takashikami@gmail.com>
2014-05-23 19:41:39 +0900

    make variant

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :set_variant
+
+  private
+
+  def set_variant
+    p request.user_agent
+    request.variant = :sp if request.user_agent =~ /Android|iPad|iPhone/
+  end
 end
