ec8117b83c3c8e5f2e0aae780b8b7507cd1705fb
Takashi KAMI <takashikami@gmail.com>
2014-06-02 17:13:19 +0900

    mkdir /data/blabo/shared/files on environment.rb only ey

--- config/environment.rb
+++ config/environment.rb
@@ -4,4 +4,4 @@ require File.expand_path('../application', __FILE__)
 # Initialize the Rails application.
 Rails.application.initialize!
 
-FileUtils.mkdir_p('/data/blabo/shared/files')
+FileUtils.mkdir_p('/data/blabo/shared/files') if ENV['USER'] == 'deploy'
